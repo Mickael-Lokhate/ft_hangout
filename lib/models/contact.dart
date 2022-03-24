eb229f8fc30fa0c99aee23640f5ebfbe89a098a2
Mickael Lokhate <lokhatemickael@gmail.com>
2022-03-24 10:58:05 +0100

    Home page display list of all contacts (not tappable now)

---
+++ lib/models/contact.dart
@@ -0,0 +1,40 @@
+class Contact {
+  String    name; 
+  String    phonenumber;
+  String?   lastname;
+  String?   email;
+  String?   imageUrl;
+  
+  Contact(this.name, this.phonenumber, [this.lastname, this.email, this.imageUrl]);
+}
+
+List<Contact> dummyContactList = [
+  Contact(
+    'Micka',
+    '0781635945',
+    'Lokhate',
+    'lokhatemickael@gmail.com'
+  ),
+  Contact(
+    'John',
+    '0111111111',
+    'Doe',
+    'johndoe@gmail.com'
+  ),
+  Contact(
+    'Test',
+    '022222222',
+  ),
+  Contact(
+    'Hello',
+    '0333333333',
+  ),
+  Contact(
+    'World',
+    '0444444444',
+  ),
+  Contact(
+    'Here',
+    '0555555555',
+  ),
+];
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,86 +1,58 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+import '../models/contact.dart';
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class ContactList extends StatefulWidget {
+  const ContactList({ Key? key }) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _ContactListState createState() => _ContactListState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _ContactListState extends State<ContactList> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('ft_hangout'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: _buildList(),
     );
+  }
+
+  Widget _buildList() {
+    if (dummyContactList.isNotEmpty) {
+      return ListView.separated(
+        padding: const EdgeInsets.all(8),
+        itemCount: dummyContactList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return _buildRowContact(dummyContactList[index]);
+        },
+        separatorBuilder: (BuildContext context, int index) => const Divider(
+          color: Colors.grey,
+        ),
+      );
+    } else {
+      return const Center(child: Text('Add your first contact with the + button'),);
+    }
+  }
+
+  Widget _buildRowContact(Contact currentContact) {
+      return ListTile(
+        leading: Container(
+          width: 64,
+          height: 64,
+          padding: const EdgeInsets.symmetric(vertical: 4.0),
+          alignment: Alignment.center,
+          child: const Icon(Icons.person, size: 50,),
+        ),
+        title: Text(
+          currentContact.name,
+          style: const TextStyle(fontWeight: FontWeight.bold),
+        ),
+        subtitle: Text(currentContact.phonenumber),
+      );
   }
 }
